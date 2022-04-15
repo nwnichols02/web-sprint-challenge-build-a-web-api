291f87d686e4a6492c1a8c5fef7b2f25b530ff36
Nathan Nichols <nwnichols02@gmail.com>
2022-04-15 09:29:38 -0600

    aint workin yet

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,32 @@
 // Write your "projects" router here!
+const express = require("express");
+
+//import middleware functions
+
+const Projects = require('../projects/projects-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then((projects) => {
+            res.status(200).json(projects);
+        })
+        .catch(err => {
+            console.log('im broken dummy')
+        })
+})
+
+// router.get('/:id', (req, res, next) => {
+//     Project.get(req.params.id, req.body)
+//         .then((projects) => {
+//             if(projects){
+//                 res.status(200).json(projects)
+//             } else {
+//                 res.status(404).json({message: "The project with the specified ID does not exist"})
+//             }
+//         })
+//         .catch(next)
+// })
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,5 +1,17 @@
-const express = require('express');
+const express = require("express");
+const projectsRouter = require("./projects/projects-router");
+const actionRouter = require("./action/actions-router");
+
 const server = express();
+
+server.use(express.json());
+
+server.use("/api/projects", projectsRouter);
+server.use("/api/actions", actionRouter);
+
+server.get("/", (req, res) => {
+  res.send("<h1>am i really working tho??</h1>");
+});
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
